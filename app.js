595a25686c34262026bc173a118b9b430557a9de
jomarMeza <mezaquintoabdiel@gmail.com>
2023-11-20 16:05:30 -0500

    listo

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(bodyparser.json({limit: '50mb', extended: true}));
 
 
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*'); 
+    res.header('Access-Control-Allow-Origin','https://administrador-88894.web.app'); 
     res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, OPTIONS');
     res.header('Allow','GET, PUT, POST, DELETE, OPTIONS');
